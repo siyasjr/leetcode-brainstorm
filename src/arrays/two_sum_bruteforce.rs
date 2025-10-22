ea1788ae189c1776b44dcfbb0477e3440e843436
siyasjr <siyasjr@gmail.com>
2025-10-22 23:49:48 +0530

    two sums using bruteforce method . O(n2)

---
+++ src/arrays/two_sum_bruteforce.rs
@@ -0,0 +1,11 @@
+fn two_sum_bruteforce(nums: Vec<i32>, target: i32) -> Vec<usize> {
+    for i in 0..nums.len() {
+        for j in i + 1..nums.len() {
+            if nums[i] + nums[j] == target {
+                return vec![i, j];
+            }
+        }
+    }
+    vec![]
+}
+
