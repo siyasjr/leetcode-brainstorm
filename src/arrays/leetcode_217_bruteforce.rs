41813f2db3157c163ac514567343f78a36b93b8c
siyasjr <siyasjr@gmail.com>
2025-10-23 23:42:03 +0530

    Compare every pair of elements.

---
+++ src/arrays/leetcode_217_bruteforce.rs
@@ -0,0 +1,10 @@
+fn contains_duplicate(nums: Vec<i32>) -> bool {
+    for i in 0..nums.len() {
+        for j in i + 1..nums.len() {
+            if nums[i] == nums[j] {
+                return true;
+            }
+        }
+    }
+    false
+}
