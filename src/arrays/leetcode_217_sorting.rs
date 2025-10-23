f9e353a6e08f479d13c1ef4756a0fe5863f20a0f
siyasjr <siyasjr@gmail.com>
2025-10-23 23:43:29 +0530

    Sort the array, then check if any adjacent elements are equal.

---
+++ src/arrays/leetcode_217_sorting.rs
@@ -0,0 +1,9 @@
+fn contains_duplicate(mut nums: Vec<i32>) -> bool {
+    nums.sort();
+    for i in 0..nums.len() - 1 {
+        if nums[i] == nums[i + 1] {
+            return true;
+        }
+    }
+    false
+}
