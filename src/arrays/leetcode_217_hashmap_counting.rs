377328f2bfe1b0f14433e7745ad0d70dbad9b03c
siyasjr <siyasjr@gmail.com>
2025-10-23 23:46:34 +0530

    Use a HashMap to count occurrences of each number.

---
+++ src/arrays/leetcode_217_hashmap_counting.rs
@@ -0,0 +1,12 @@
+use std::collections::HashMap;
+
+fn contains_duplicate(nums: Vec<i32>) -> bool {
+    let mut map = HashMap::new();
+    for &num in &nums {
+        *map.entry(num).or_insert(0) += 1;
+        if map[&num] > 1 {
+            return true;
+        }
+    }
+    false
+}
