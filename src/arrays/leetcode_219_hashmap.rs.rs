ae1230e6965719259343b1862de2dee565c22f1a
siyasjr <siyasjr@gmail.com>
2025-10-24 23:57:53 +0530

    Store the last index of each number.

---
+++ src/arrays/leetcode_219_hashmap.rs.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
+        let mut map = HashMap::new();
+
+        for (i, &num) in nums.iter().enumerate() {
+            if let Some(&prev_i) = map.get(&num) {
+                if (i as i32 - prev_i as i32).abs() <= k {
+                    return true;
+                }
+            }
+            map.insert(num, i as i32);
+        }
+        false
+    }
+}
