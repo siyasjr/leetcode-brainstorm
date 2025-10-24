64b2f64fd3f1fc004bb0fd558e53652459395dbf
siyasjr <siyasjr@gmail.com>
2025-10-25 00:00:01 +0530

    Keep a window of last k elements using a set.

---
+++ src/arrays/leetcode_219_hashset.rs.rs
@@ -0,0 +1,18 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
+        let mut set = HashSet::new();
+
+        for i in 0..nums.len() {
+            if set.contains(&nums[i]) {
+                return true;
+            }
+            set.insert(nums[i]);
+            if set.len() > k as usize {
+                set.remove(&nums[i - k as usize]);
+            }
+        }
+        false
+    }
+}
