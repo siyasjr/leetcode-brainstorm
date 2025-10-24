2d4d99d858585fb064c3901a2d84d0a5426ef7e8
siyasjr <siyasjr@gmail.com>
2025-10-24 23:56:48 +0530

    Check all pairs within distance k.

---
+++ src/arrays/leetcode_219_bruteforce.rs.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
+        let n = nums.len();
+        for i in 0..n {
+            for j in i+1..=(i + k as usize).min(n - 1) {
+                if nums[i] == nums[j] {
+                    return true;
+                }
+            }
+        }
+        false
+    }
+}
