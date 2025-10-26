d17c7d8e2d74d79614771cab9b0fbe252c9a0df5
siyasjr <siyasjr@gmail.com>
2025-10-26 23:59:18 +0530

    Prefix + Suffix Arrays

---
+++ src/arrays/leetcode_238_prefix_suffix_arrays.rs
@@ -0,0 +1,22 @@
+impl Solution {
+    pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+        let n = nums.len();
+        let mut prefix = vec![1; n];
+        let mut suffix = vec![1; n];
+        let mut res = vec![1; n];
+
+        for i in 1..n {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+
+        for i in (0..n - 2).rev() {
+            suffix[i] = suffix[i + 1] * nums[i + 1];
+        }
+
+        for i in 0..n {
+            res[i] = prefix[i] * suffix[i];
+        }
+
+        res
+    }
+}
