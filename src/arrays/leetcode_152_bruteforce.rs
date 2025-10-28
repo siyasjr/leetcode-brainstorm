b8ce1a47382bb2cf64c0136e9a32fce765215f1e
siyasjr <siyasjr@gmail.com>
2025-10-28 23:56:57 +0530

    Compute product of every possible subarray.

---
+++ src/arrays/leetcode_152_bruteforce.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn max_product(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        let mut max_prod = i32::MIN;
+
+        for i in 0..n {
+            let mut prod = 1;
+            for j in i..n {
+                prod *= nums[j];
+                max_prod = max_prod.max(prod);
+            }
+        }
+
+        max_prod
+    }
+}
