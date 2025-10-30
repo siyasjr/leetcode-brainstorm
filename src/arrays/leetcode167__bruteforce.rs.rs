234c1b71a4a14eaa2d4b2a1106baeb65a6781204
siyasjr <siyasjr@gmail.com>
2025-10-30 23:59:45 +0530

    bruteforce

---
+++ src/arrays/leetcode167__bruteforce.rs.rs
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+        for i in 0..numbers.len() {
+            for j in i + 1..numbers.len() {
+                if numbers[i] + numbers[j] == target {
+                    return vec![(i + 1) as i32, (j + 1) as i32];
+                }
+            }
+        }
+        vec![]
+    }
+}
