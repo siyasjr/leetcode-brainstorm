934c0171a649971765405b35bbe8e49d5a04e356
siyasjr <siyasjr@gmail.com>
2025-11-07 23:30:55 +0530

    Compare every pair (i, j) and compute the area.

---
+++ src/arrays/leetcode_11_bruteforce.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        let n = height.len();
+        let mut max_area = 0;
+
+        for i in 0..n {
+            for j in i + 1..n {
+                let area = (j - i) as i32 * height[i].min(height[j]);
+                max_area = max_area.max(area);
+            }
+        }
+        max_area
+    }
+}
