77e185cba589d58c005ba60328b1ec0be9156f0b
siyasjr <siyasjr@gmail.com>
2025-11-07 23:33:30 +0530

    same logic as two pointer

---
+++ src/arrays/leetcode_11_iteratorstyle.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        let (mut l, mut r) = (0, height.len() - 1);
+        let mut ans = 0;
+
+        while l < r {
+            ans = ans.max((r - l) as i32 * height[l].min(height[r]));
+            if height[l] < height[r] {
+                l += 1;
+            } else {
+                r -= 1;
+            }
+        }
+        ans
+    }
+}
