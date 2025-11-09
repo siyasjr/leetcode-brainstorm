aa1be25e3643ff6697e720b9f9d15a432336a1c2
siyasjr <siyasjr@gmail.com>
2025-11-09 22:06:52 +0530

    using dynamic programming

---
+++ src/Dynamic_pgm /leetcode_953_dynamicpgm.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn climb_stairs(n: i32) -> i32 {
+        if n <= 2 {
+            return n;
+        }
+
+        let mut dp = vec![0; (n + 1) as usize];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for i in 3..=n as usize {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        dp[n as usize]
+    }
+}
