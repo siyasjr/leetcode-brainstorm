f5bd3ab5b9e9494ef6dd151dec47e53fc4aa7b76
siyasjr <siyasjr@gmail.com>
2025-11-09 22:05:33 +0530

    recursion

---
+++ src/Dynamic_pgm /leetcode_953_recursion.rs
@@ -0,0 +1,20 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn climb_stairs(n: i32) -> i32 {
+        fn helper(n: i32, memo: &mut HashMap<i32, i32>) -> i32 {
+            if n <= 2 {
+                return n;
+            }
+            if let Some(&val) = memo.get(&n) {
+                return val;
+            }
+            let val = helper(n - 1, memo) + helper(n - 2, memo);
+            memo.insert(n, val);
+            val
+        }
+
+        let mut memo = HashMap::new();
+        helper(n, &mut memo)
+    }
+}
