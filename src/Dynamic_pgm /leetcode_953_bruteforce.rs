0f6510b8e8f7e682bf707966febbfa38934c54dd
siyasjr <siyasjr@gmail.com>
2025-11-09 22:04:28 +0530

    bruteforce

---
+++ src/Dynamic_pgm /leetcode_953_bruteforce.rs
@@ -0,0 +1,8 @@
+impl Solution {
+    pub fn climb_stairs(n: i32) -> i32 {
+        if n <= 2 {
+            return n;
+        }
+        Self::climb_stairs(n - 1) + Self::climb_stairs(n - 2)
+    }
+}
