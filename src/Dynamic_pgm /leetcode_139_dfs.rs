89b8a09a4393458d1aca07c3e6682ac828b18c35
siyasjr <siyasjr@gmail.com>
2025-11-19 23:56:02 +0530

    Top-Down Dynamic Programming

---
+++ src/Dynamic_pgm /leetcode_139_dfs.rs
@@ -0,0 +1,24 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn word_break(s: String, word_dict: Vec<String>) -> bool {
+        let set: HashSet<String> = word_dict.into_iter().collect();
+        let mut memo = vec![None; s.len() + 1];
+
+        fn dfs(i: usize, s: &str, set: &HashSet<String>, memo: &mut Vec<Option<bool>>) -> bool {
+            if i == s.len() { return true; }
+            if let Some(v) = memo[i] { return v; }
+
+            for j in i+1..=s.len() {
+                if set.contains(&s[i..j]) && dfs(j, s, set, memo) {
+                    memo[i] = Some(true);
+                    return true;
+                }
+            }
+            memo[i] = Some(false);
+            false
+        }
+
+        dfs(0, &s, &set, &mut memo)
+    }
+}
