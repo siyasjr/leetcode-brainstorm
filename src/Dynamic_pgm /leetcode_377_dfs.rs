1876072e4420aaebd75e4c4da49b2ac88ed0322d
siyasjr <siyasjr@gmail.com>
2025-11-20 23:35:24 +0530

    classic efficient recursion-based solution.

---
+++ src/Dynamic_pgm /leetcode_377_dfs.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn combination_sum4(nums: Vec<i32>, target: i32) -> i32 {
+        let mut memo = vec![-1; (target + 1) as usize];
+
+        fn dfs(nums: &Vec<i32>, t: i32, memo: &mut Vec<i32>) -> i32 {
+            if t == 0 { return 1; }
+            if t < 0 { return 0; }
+
+            let idx = t as usize;
+            if memo[idx] != -1 {
+                return memo[idx];
+            }
+
+            let mut ways = 0;
+            for &n in nums {
+                ways += dfs(nums, t - n, memo);
+            }
+
+            memo[idx] = ways;
+            ways
+        }
+
+        dfs(&nums, target, &mut memo)
+    }
+}
