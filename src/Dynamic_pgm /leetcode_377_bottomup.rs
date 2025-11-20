741504933ecb98d94985700be6bb41c41ed15003
siyasjr <siyasjr@gmail.com>
2025-11-20 23:36:44 +0530

    counts number of ways to reach every sum from 0..target

---
+++ src/Dynamic_pgm /leetcode_377_bottomup.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn combination_sum4(nums: Vec<i32>, target: i32) -> i32 {
+        let mut dp = vec![0; (target + 1) as usize];
+        dp[0] = 1;
+
+        for t in 1..=target as usize {
+            for &n in &nums {
+                if n as usize <= t {
+                    dp[t] += dp[t - n as usize];
+                }
+            }
+        }
+
+        dp[target as usize]
+    }
+}
