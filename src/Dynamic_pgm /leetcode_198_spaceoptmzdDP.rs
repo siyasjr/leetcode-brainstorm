84d72a42e015c3fc54693b866ada732c44790452
siyasjr <siyasjr@gmail.com>
2025-11-21 23:46:51 +0530

    most idiomatic and optimal solution

---
+++ src/Dynamic_pgm /leetcode_198_spaceoptmzdDP.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn rob(nums: Vec<i32>) -> i32 {
+        let mut prev1 = 0; // dp[i-1]
+        let mut prev2 = 0; // dp[i-2]
+
+        for n in nums {
+            let rob = prev2 + n;
+            let skip = prev1;
+            let cur = rob.max(skip);
+
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        prev1
+    }
+}
