067a9f847a94a3f147a0b89845afc4805624d6c6
siyasjr <siyasjr@gmail.com>
2025-11-23 22:55:10 +0530

    bruteforce recursion

---
+++ src/Dynamic_pgm /leetcode_91_bruteforce.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn num_decodings(s: String) -> i32 {
+        fn dfs(i: usize, s: &str) -> i32 {
+            if i == s.len() { return 1; }
+            if &s[i..i+1] == "0" { return 0; }
+
+            let mut ways = dfs(i + 1, s); // single digit
+
+            // two digits
+            if i + 2 <= s.len() && s[i..i+2].parse::<i32>().unwrap() <= 26 {
+                ways += dfs(i + 2, s);
+            }
+            ways
+        }
+
+        dfs(0, &s)
+    }
+}
