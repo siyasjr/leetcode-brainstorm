768705c0c1dd07902d8efc2495544eff7143497d
siyasjr <siyasjr@gmail.com>
2025-11-24 23:57:50 +0530

    bruteforce

---
+++ src/Dynamic_pgm /leetcode_62_bruteforce.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn unique_paths(m: i32, n: i32) -> i32 {
+        fn dfs(i: i32, j: i32, m: i32, n: i32) -> i32 {
+            if i == m - 1 && j == n - 1 { return 1; }
+            if i >= m || j >= n { return 0; }
+            dfs(i + 1, j, m, n) + dfs(i, j + 1, m, n)
+        }
+
+        dfs(0, 0, m, n)
+    }
+}
