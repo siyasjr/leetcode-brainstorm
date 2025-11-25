e535f2160033dcc5e5147f1e7f658dc5d9023322
siyasjr <siyasjr@gmail.com>
2025-11-25 23:31:11 +0530

    iterative DP

---
+++ src/Dynamic_pgm /leetcode_118_iterative.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
+        let n = num_rows as usize;
+        let mut res = Vec::with_capacity(n);
+
+        for i in 0..n {
+            let mut row = vec![1; i + 1];
+
+            for j in 1..i {
+                row[j] = res[i - 1][j - 1] + res[i - 1][j];
+            }
+
+            res.push(row);
+        }
+
+        res
+    }
+}
