20eca86c0dba2ca91474718dafbcbdb111e455ec
siyasjr <siyasjr@gmail.com>
2025-11-25 23:32:24 +0530

    returns full triangle but uses less temporary memory

---
+++ src/Dynamic_pgm /leetcode_118_spaceoptmzd.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
+        let n = num_rows as usize;
+        let mut res = Vec::with_capacity(n);
+        let mut prev = vec![];
+
+        for i in 0..n {
+            let mut row = vec![1; i + 1];
+
+            for j in 1..i {
+                row[j] = prev[j - 1] + prev[j];
+            }
+
+            prev = row.clone();
+            res.push(row);
+        }
+
+        res
+    }
+}
