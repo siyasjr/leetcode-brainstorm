98d126d7310ac5c1ec9f45208fc715473fedf3ea
siyasjr <siyasjr@gmail.com>
2025-11-26 23:49:28 +0530

    Using dynamic programing

---
+++ src/Dynamic_pgm /leetcode_22_dynpgm.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn generate_parenthesis(n: i32) -> Vec<String> {
+        let n = n as usize;
+        let mut dp: Vec<Vec<String>> = vec![vec![]; n + 1];
+        dp[0] = vec![String::from("")];
+
+        for i in 1..=n {
+            let mut curr = Vec::new();
+            for left in 0..i {
+                for a in dp[left].iter() {
+                    for b in dp[i - 1 - left].iter() {
+                        curr.push(format!("({}){}", a, b));
+                    }
+                }
+            }
+            dp[i] = curr;
+        }
+
+        dp[n].clone()
+    }
+}
