47ecf325065ab06fbf14ab82ad8964487e6035a2
siyasjr <siyasjr@gmail.com>
2025-11-30 23:11:13 +0530

    treat each index as a node, edges represent jumps.

---
+++ src/Dynamic_pgm /leetcode_45_BFS.rs
@@ -0,0 +1,33 @@
+use std::collections::VecDeque;
+
+impl Solution {
+    pub fn jump(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        if n <= 1 {
+            return 0;
+        }
+
+        let mut visited = vec![false; n];
+        visited[0] = true;
+
+        let mut queue = VecDeque::new();
+        queue.push_back((0, 0)); // (index, steps)
+
+        while let Some((i, steps)) = queue.pop_front() {
+            if i == n - 1 {
+                return steps;
+            }
+
+            let max_jump = nums[i] as usize;
+
+            for j in i+1..=i+max_jump {
+                if j < n && !visited[j] {
+                    visited[j] = true;
+                    queue.push_back((j, steps + 1));
+                }
+            }
+        }
+
+        0
+    }
+}
